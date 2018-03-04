b73adf2b9b27aa7a202a465df8f365074f6b9857
Amit <amitsharma.sgr99@gmail.com>
2018-03-05 02:55:26 +0530

    block offensive words validator in forms.py

--- forumapp/forms.py
+++ forumapp/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 from .models import Comment,Question
+from django.core import validators
+
+
+# Block Offensive Words Validator
+def block_offensive_words(value):
+    offensive_words = ['fuck','dickhole','dick','sex','fuckyou','bitch','idiot',]
+    temp = value.split(" ")
+    for i in temp:
+        if i in offensive_words:
+            raise forms.ValidationError("You Cannot post with BAD WORDS")
 
 
 class CommentForm(forms.ModelForm):
@@ -9,8 +19,7 @@ class CommentForm(forms.ModelForm):
 
 
 class QuestionForm(forms.ModelForm):
-    content = forms.CharField(max_length=1000,widget=forms.Textarea)
+    content = forms.CharField(max_length=1000,widget=forms.Textarea,validators=[block_offensive_words,])
     class Meta:
         model = Question
         fields = ('title', 'content',)
-
